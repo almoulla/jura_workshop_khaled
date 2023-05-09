e380d2459057a9d7f1e85f80a7c6d13a6b50c9af
Khaled Al Moulla <khaled.almoulla@gmail.com>
2023-05-09 12:11:29 +0200

    Updated README.md

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 ### JURA IV python example, May 09, 2023
 
+## Installation
+
+```
+git clone https://github.com/almoulla/jura_workshop_khaled
+cd jura_workshop_khaled/
+pip install .
+```
+
+## Quickstart
+
 ```
 from rv_curve_jura.rv_curve_module import rv_curve_class
 ```
--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="rv_curve_jura",
     version="0.1",
     author="Khaled Al Moulla",
     author_email="khaled.almoulla@gmail.com",
     description="Plot all RV curves that you want!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/almoulla/jura_workshop_khaled",
     packages=setuptools.find_packages(),
     install_requires=["numpy","matplotlib"],
     classifiers=[
